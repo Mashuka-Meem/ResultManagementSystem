9dce58f437a7371090803e0937249ebc814952ca
Mashuka Meem <41076458+Mashuka-Meem@users.noreply.github.com>
2019-01-10 18:44:43 +0600

    Add files via upload

---
+++ ResultManagementSystem/Data/TestContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ResultManagementSystem.Data
+{
+    public class TestContext:DbContext
+    {
+        public TestContext(DbContextOptions<TestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ResultManagementSystem.Models.Actor> Actor { get; set; }
+        public DbSet<ResultManagementSystem.Models.Login> Login { get; set; }
+        public DbSet<ResultManagementSystem.Models.Student> Student { get; set; }
+        public DbSet<ResultManagementSystem.Models.Teacher> Teacher { get; set; }
+        public DbSet<ResultManagementSystem.Models.Course> Course { get; set; }
+        public DbSet<ResultManagementSystem.Models.Classes> Classes { get; set; }
+        public DbSet<ResultManagementSystem.Models.Marks> Marks { get; set; }
+    }
+}
